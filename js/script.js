d48613f0a059f5951054b74a2432e131593b2ae1
Ipek Canli <119710326+i99c@users.noreply.github.com>
2024-01-16 15:29:09 +0300

    responsive hariç site tamamlandı

--- js/script.js
+++ js/script.js
@@ -40,8 +40,27 @@ window.onscroll = () => {
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 
-
-
 }
 
+// dark light mode
 
+let darkModeIcon = document.querySelector('#darkMode-icon')
+
+darkModeIcon.onclick = () => {
+    darkModeIcon.classList.toggle('bx-sun');
+    document.body.classList.toggle('dark-mode');
+}
+
+// scroll reveal
+ScrollReveal({
+    // reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img img, .services-container, .portfolio-box, .contact form ', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img img  ', { origin: 'left' });
+ScrollReveal().reveal('.home-content h3,.home-content p, .about-content  ', { origin: 'right' });
