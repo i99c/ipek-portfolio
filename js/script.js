43cb488d6fc758288674daa4b37c1bde3a00d9e8
Ipek Canli <119710326+i99c@users.noreply.github.com>
2024-01-16 03:11:31 +0300

    responsive yapıldı

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+// menu icon navbar 
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
+
 // scroll sections active
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -22,4 +34,14 @@ window.onscroll = () => {
     // sticky navbar
     let header = document.querySelector('.header');
     header.classList.toggle('sticky', window.scrollY > 100)
+
+    // remove menu icon navbar when click navbar link(scroll)
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+
+
+
 }
+
+
